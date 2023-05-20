cefe67bb03c715a0df1a597f92a9b4b0c902f319
Andrei2458 <andrei.vacariu@student.upt.ro>
2023-05-20 22:09:23 +0300

    site for stv test

---
+++ extra/script.js
@@ -0,0 +1,23 @@
+function calculate() {
+    var d = parseFloat(document.getElementById('zi').value);
+    var m = parseFloat(document.getElementById('luna').value);
+    
+    d_1 = Math.floor(d / 10);
+
+    var X = (5 + m + d_1)  % 10;
+    var Y = (7 + d + m * 5)  % 10
+    var Z = (d + 20) % 10;
+    L1 = 25 + 10 * m + 30 * d_1 // max 25 + 120 + 90 = 235 // min 25 + 10 + 0 = 35
+    L2 = 335 + 20*m + d // min 335+20+1 = 356 // max 335 + 240 + 31 = 606
+
+
+    
+    
+
+    document.getElementById('L1').innerText = "L1: " + L1;
+    document.getElementById('L2').innerText = "L2: " + L2;
+    document.getElementById('X').innerText = "X: " + X;
+    document.getElementById('Y').innerText = "Y: " + Y;
+    document.getElementById('Z').innerText = "Z: " + Z;
+  }
+  
